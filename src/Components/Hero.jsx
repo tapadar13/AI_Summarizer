44932f7bf157a1c7736448f0ea40dbba3271f3c2
tapadar13 <monsurismyname@gmail.com>
2023-04-23 18:30:50 +0530

    navbar implemented

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,22 @@
+import { logo } from "../assets";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-100 flex justify-center items-center flex-col">
+      <nav className="w-full flex justify-center items-center mb-10 pt-3">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            window.open();
+          }}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+    </header>
+  );
 };
 
 export default Hero;
