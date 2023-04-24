cc8084b1a8727d7f167e29a2523bd244362d41ae
tapadar13 <monsurismyname@gmail.com>
2023-04-24 21:53:15 +0530

    configured our store

--- src/services/store.js
+++ src/services/store.js
@@ -1,6 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
+import { articleAPI } from "./article";
+
 export const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  reducer: {
+    [articleAPI.reducerPath]: articleAPI.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(articleAPI.middleware),
 });
